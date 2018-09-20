29c6bce3634cb938921ba5f6ff0c7d31fe98f2af
liuuuu <lkarim.mac@gmail.com>
2018-09-20 11:59:02 +0800

    Init commit

---
+++ app/src/main/java/com/liuuuu/datapersistdemo/MainActivity.java
@@ -0,0 +1,31 @@
+package com.liuuuu.datapersistdemo;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends ListActivity {
+
+    Class[] classes = {};
+    String[] titles = {};
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, android.R.id.text1, titles);
+
+        setListAdapter(arrayAdapter);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        Intent intent = new Intent(this, classes[position]);
+        startActivity(intent);
+    }
+}
